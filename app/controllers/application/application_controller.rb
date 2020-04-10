e94eb40cec11d80163bec83d50b0905c3bae34c1
Tony Reyes <antoniomanuelreyes@gmail.com>
2020-04-10 05:52:33 -0400

    Added a 404 error.

--- app/controllers/application/application_controller.rb
+++ app/controllers/application/application_controller.rb
@@ -8,6 +8,10 @@ class Application < Sinatra::Application
     File.read(File.join('public', 'index.html'))
   end
 
+  not_found do
+    File.read(File.join('public', '404.html'))
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $PROGRAM_NAME
 end
