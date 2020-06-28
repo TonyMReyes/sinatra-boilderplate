55929dd06bc2fb513fc9b24c961e9da6811a2640
Tony Reyes <antoniomanuelreyes@gmail.com>
2020-06-28 11:46:13 -0400

    added Active Record comments.

--- config.ru
+++ config.ru
@@ -1,4 +1,19 @@
 # config.ru (run with rackup)
 require './app/controllers/application/application_controller'
+require_relative './config/environment'
+
+=begin
+if ActiveRecord::Migrator.needs_migration?
+  raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
+end
+=end
+
+# Allows the method to be overridden if params[:_method] is set. This is the middleware which supports the PUT
+# and DELETE HTTP method types.
+
+# use Rack::MethodOverride
+# use OtherController1
+# use OtherController2
+# use OtherController3
 
 run Application
--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
+# “double-pipe equals” is an operator that assigns a value, much like = or our classic assignment operator,
+# but will only complete the assignment if the left side of our operation returns false or nil.
 ENV['SINATRA_ENV'] ||= 'development'
 
 require 'bundler/setup'
 Bundler.require(:default,ENV['SINATRA_ENV'])
+
+# Database setup
+# TODO: add ActiveRecord to Gemfile
+=begin
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/development.sqlite"
+)
+=end
 
 require '../app/controllers/application/application_controller'
 require_all 'app'
